9b24bbc3a8de57bc4466ba5ca0f00ab55e47aa6e
MihailTsankov77 <71463879+MihailTsankov77@users.noreply.github.com>
2020-10-29 18:41:58 +0200

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,21 @@
+package klas11g;
+
+public class Circle {
+	private double rad;
+		
+	public Circle(double _rad) {
+		rad = _rad;
+	}
+	
+	public double getArea() {
+		return Math.PI*rad*rad;
+	}
+	
+	public double getPerimeter() {
+		return Math.PI*2*rad;
+	}
+	
+	public void setRad(double _newRad) {
+		rad = _newRad;
+	}
+}
